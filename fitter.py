db6c021a00d340d337e69864bbc39ed4e0ef46bf
YMTheory <932899789@qq.com>
2024-08-12 12:48:38 -0700

    Rate-only Asimov dchi2 calculation.

--- fitter.py
+++ fitter.py
@@ -2,7 +2,8 @@ import os
 import numpy as np
 from iminuit import cost, Minuit
 import histlite as hl
-import pickle
+import sys
+import h5py as h5
 
 from MC_generator import MC_generator
 
@@ -28,161 +29,89 @@ class fitter:
         self.dataset = []
         self.PDF  = None
         self.data_filename = None
+        self.load_data_flag = False
         self.pdf_filename = None
+        self.load_pdf_flag = False
         
 
-    def _set_datafilename(self, filename):
-        self.data_filename = filename
-        
-    def _set_pdffilename(self, filename):
-        self.pdf_filename = filename
+        self.data_dm2 = 0.0
+        self.data_sin2 = 0.0
+        self.fit_dm2 = 0.0
+        self.fit_sin2 = 0.0
 
-        
-    def _set_dm2(self, dm2):
-        self.MC_gen.dm2 = dm2
-        self.dm2 = dm2
+    ## Setters:
+    def _set_data_dm2(self, val):
+        self.data_dm2 = val
     
-    def _set_sin2theta_square(self, sin2):
-        self.MC_gen.sin2theta_square = sin2
-        self.sin2theta_square = sin2
+    def _set_data_sin2(self, val):
+        self.data_sin2 = val
+        
+    def _set_fit_dm2(self, val):
+        self.fit_dm2 = val
+        
+    def _set_fit_sin2(self, val):
+        self.fit_sin2 = val
 
+    def _set_data_filename(self, name):
+        self.data_filename = name
         
+    def _set_pdf_filename(self, name):
+        self.pdf_filename = name
+   ######################################################################## 
         
-    def generate_PDF(self, dm2, sin2theta_square, Enu):
-        self.MC_gen.dm2 = dm2
-        self.MC_gen.sin2theta_square = sin2theta_square
+    
+    ## Gettters:
+    def _get_data_dm2(self):
+        return self.data_dm2
+    
+    def _get_data_sin2(self):
+        return self.data_sin2
+    
+    def _get_fit_dm2(self):
+        return self.fit_dm2
+    
+    def _get_fit_sin2(self):
+        return self.fit_sin2
+
+   ######################################################################## 
         
-        self.MC_gen.n_events = 1e6 # Hard-coded for now, enough statistics.
-        self.scale_count_flag = False
-        _, bl = self.MC_gen.generate_oscillate_Asimov_dataset(Enu)
-        
-        h = hl.hist(bl, bins=self.n_Lbins, range=(self.Lmin, self.Lmax))
-        h_norm = h.normalize()
-        return h_norm
+    def load_data_file(self, evno=[]):
+        if not os.path.exists(self.data_filename):
+            print(f'{self.data_filename} does not exist :(')
+            sys.exit(-1)
+
+        with h5.File(self.data_filename, 'r') as f:
+
+            if len(evno) == []:
+                dataset_names = f.keys()
+                for name in dataset_names:
+                    self.dataset.append( f[name][:]) 
+            
+            else:
+                for i in evno:
+                    name = f'event{i}'
+                    if name not in f.keys():
+                        continue
+                    self.dataset.append( f[name][:])
+            
+                    
+                    
     
     
-    def write_PDFs_intoFile(self, PDFs, filename):
-        write_file = True
-        if os.path.exists(filename):
-            write_file = False
-            user_input = input(f"{filename} already exists, do you want to overwrite it?").strip().lower()
-            if user_input == 'yes':
-                write_file = True        
-        if write_file:
-            with open(filename, 'wb') as file:
-                pickle.dump(PDFs, file)
-
-
-    def load_PDFs_fromFile(self):
-        filename = self.pdf_filename
-        try:
-            with open(filename, 'rb') as file:
-                self.PDF = pickle.load(file)
-            return self.PDF
-        except:
-            print(f"{filename} does not exists.")
-    
-    
-    def generate_dataset(self, dm2, sin2theta_square, Enu):
-        self.MC_gen.dm2 = dm2
-        self.MC_gen.sin2theta_square = sin2theta_square
-        if not self.scale_count_flag:
-            self.MC_gen.scale_counts()
-            self.scale_count_flag = True
+    def calculate_rateonly_dchi2_asimov(self):
+        ## There is actually no only fitting, just calculating rate-only delta_chi2 for (sin2, dm2) pairs.
+        # Fitting histogram
+        h_fit = self.MC_gen.generate_asimov_dataset(data_dm2=self.fit_dm2, data_sin2=self.fit_sin2, )
+        fit_nevt = np.sum( h_fit.values )
         
-        _, bl = self.MC_gen.generate_oscillate_Asimov_dataset(Enu)
+        # Data asimov histogram
+        h_data = self.MC_gen.generate_asimov_dataset(data_dm2=self.data_dm2, data_sin2=self.data_sin2, )
+        data_nevt = np.sum( h_data.values )
+
+        # For now, only consider Poisson statistics fluctuation.
+        sigma_data = np.sqrt( data_nevt )
         
-        return bl
+        chi2 = (fit_nevt - data_nevt)**2 / sigma_data**2
 
-    def write_data_histo_intoFiles(self, bls, filename):
-        write_file = True
-        if os.path.exists(filename):
-            write_file = False
-            user_input = input(f"{filename} already exists, do you want to overwrite it?").strip().lower()
-            if user_input == 'yes':
-                write_file = True        
-        if write_file:
-            hists = []
-            for bl in bls:
-                h = hl.hist(bl, bins=self.n_Lbins, range=(self.Lmin, self.Lmax))
-                hists.append(h)
-            with open(filename , 'wb') as file:
-                pickle.dump(hists, file)
-            
-    
-    def load_data_fromFile(self):
-        filename = self.data_filename
-        try:
-            with open(filename, 'rb') as file:
-                self.dataset = pickle.load(file)
-            return self.dataset
-        except:
-            print(f"{filename} does not exists.")
-            
-
-    def expected_rate(self, bl, N):
-        if self.PDF is None:
-            self.load_PDFs_fromFile()
-            return N * self.PDF.get_value(bl)
-
-            
-    def fit_statiscs_only_oneEvent(self, evno):
-        if len(self.dataset) == 0:
-            self.load_data_fromFile()
-
-        if evno > len(self.dataset):
-            evno = len(self.dataset) - 1
-            
-        # The raw dataset is array, need to fit it into histograms
-        h_data = self.dataset[evno]
-        
-        c = cost.BinnedNLL(h_data.values, h_data.bins[0], self.expected_rate)
-        m = Minuit(c, N=4000,)
-        m.limits['N'] = (3000, 5000)
-        m.migrad()
-        
-        return m
-            
-
-    def fit_statiscs_only_oneEvent_manually(self, evno, Nmin=300, Nmax=500):
-        if len(self.dataset) == 0:
-            self.load_data_fromFile()
-            
-        if evno > len(self.dataset):
-            evno = len(self.dataset) - 1
-            
-        h_data = self.dataset[evno]
-
-        def get_chi2(N):
-            chi2 = 0
-            for i in range(h_data.n_bins[0]):
-                obs = h_data.values[i]
-                err = h_data.errors[i]
-                pred = N * self.PDF.values[i]
-                if err == 0:
-                    continue
-                chi2 += (obs - pred)**2 / err**2
-
-            return chi2
-
-        # Coarse scanning:
-        step = 0.1
-        coarse_step, coarse_chi2 = np.arange(Nmin, Nmax, step), []
-        for Ni in coarse_step:
-            tmp_chi2 = get_chi2(Ni)
-            coarse_chi2.append(tmp_chi2)
-        coarse_chi2 = np.array(coarse_chi2)
-        N0 = coarse_step[np.argmin(coarse_chi2)]
-
-        # Fine scanning:
-        step = 0.01
-        fine_step, fine_chi2 = np.arange(N0-5, N0+5, step), []
-        for Ni in fine_step:
-            tmp_chi2 = get_chi2(Ni)
-            fine_chi2.append(tmp_chi2)
-        fine_chi2 = np.array(fine_chi2)
-
-        idx = np.argmin(fine_chi2)
-        return fine_step[idx], fine_chi2[idx],coarse_step, coarse_chi2, fine_step, fine_chi2
-
+        return chi2
         
